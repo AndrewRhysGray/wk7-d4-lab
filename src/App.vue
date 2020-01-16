e61ac0da604e4e8ad5cd3d706e67e6fac8718bbd
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-01-16 11:31:11 +0000

    beer array and beerDetails object runing

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      beers: [],
+      selectedBeer: {}
+    };
+  },
   components: {
     HelloWorld
+  },
+
+  mounted() {
+    fetch("https://api.punkapi.com/v2/beers")
+      .then(beersData => beersData.json())
+      .then(beers => (this.beers = beers));
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
